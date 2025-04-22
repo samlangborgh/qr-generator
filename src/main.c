f01b5fd57127ee52ace131568aad853ae18089b8
Sam Langborgh <slangborgh16@gmail.com>
2025-04-22 14:45:20 -0400

    Added options to set the desired error correction level.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "qrencode.h"
 
 int main(int argc, char** argv) {
     ErrorCorrectionLevel ecLevel = EC_H;
 
-    char helloWorld[] = "Hello, world!";
-    QR* qr = NULL;
-
-    if (argc > 1) {
-        printf("Message: %s\n", argv[1]);
-        qr = createQRCode(argv[1], ecLevel);
-    } else {
-        printf("Message: %s\n", helloWorld);
-        qr = createQRCode(helloWorld, ecLevel);
+    int opt;
+    while ((opt = getopt(argc, argv, "LMQHh")) != -1) {
+        switch (opt) {
+            case 'L':
+                ecLevel = EC_L;
+                break;
+            case 'M':
+                ecLevel = EC_M;
+                break;
+            case 'Q':
+                ecLevel = EC_Q;
+                break;
+            case 'H':
+                ecLevel = EC_H;
+                break;
+            case 'h':
+                printf("help message oh yeah\n");
+                exit(EXIT_SUCCESS);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s message [-L|-M|-Q|-H] [-h]", argv[0]);
+                exit(EXIT_FAILURE);
+        }
     }
+
+    char* message = NULL;
+    char helloWorld[] = "Hello, world!";
+
+    if (optind >= argc) {
+        // No argument provided
+        message = helloWorld;
+    } else {
+        message = argv[optind];
+    }
+
+    printf("Message: %s\n", message);
+
+    QR* qr = createQRCode(message, ecLevel);;
 
     printf("Version %d - Size: %dx%d\n", qr->version, qr->width, qr->width);
     printQR(qr);
