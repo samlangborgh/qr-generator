328db7ece5c29d8143b43064b38ec4831d898e2d
Sam Langborgh <slangborgh16@gmail.com>
2025-04-23 12:10:20 -0400

    Help option is now only --help to avoid ambiguity. Also added a proper help message.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 
 #include "qrencode.h"
 
+void printUsageMessage(const char* progName);
+void printHelpMessage(const char* progName);
+
 int main(int argc, char** argv) {
     ErrorCorrectionLevel ecLevel = EC_H;
 
@@ -12,12 +15,16 @@ int main(int argc, char** argv) {
     // If not, eventually add structured append functionality
     int opt;
     const struct option long_options[] = {
-        {"help", no_argument, NULL, 'h'},
+        {"help", no_argument, NULL, 0},
         {0, 0, 0, 0},
     };
 
-    while ((opt = getopt_long(argc, argv, "LMQHh", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "LMQH", long_options, NULL)) != -1) {
         switch (opt) {
+            case 0:
+                printHelpMessage(argv[0]);
+                exit(EXIT_SUCCESS);
+                break;
             case 'L':
                 ecLevel = EC_L;
                 break;
@@ -30,12 +37,8 @@ int main(int argc, char** argv) {
             case 'H':
                 ecLevel = EC_H;
                 break;
-            case 'h':
-                printf("help message oh yeah\n");
-                exit(EXIT_SUCCESS);
-                break;
             default:
-                fprintf(stderr, "Usage: %s message [-L|-M|-Q|-H] [-h]", argv[0]);
+                printUsageMessage(argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
@@ -62,4 +65,23 @@ int main(int argc, char** argv) {
     qr = NULL;
 
     return 0;
+}
+
+void printUsageMessage(const char* progName) {
+    fprintf(stderr, "Usage: %s message [-L|-M|-Q|-H] [--help]\n", progName);
+}
+
+void printHelpMessage(const char* progName) {
+    printUsageMessage(progName);
+    printf("\nArguments:\n");
+    printf("  message   message used to create QR code\n");
+    printf("\nOptions:\n");
+    printf("  -L        set error correction level to low (7%% of data bytes can be restored)\n");
+    printf("  -M        set error correction level to medium (15%% of data bytes can be restored)\n");
+    printf("  -Q        set error correction level to quartile (25%% of data bytes can be restored)\n");
+    printf("  -H        set error correction level to high (30%% of data bytes can be restored)\n");
+    printf("  --help    display this help message\n");
+    printf("\nNote: The default error correction level is high\n");
+    printf("\nExample:\n");
+    printf("  %s \"Hello, world!\" -L\n", progName);
 }
